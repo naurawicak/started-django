19c7b36f4fef57c20a4853a0ae37440958061299
anjlnk <ibrm632@gmail.com>
2023-03-11 02:00:40 +0700

    INITIAL COMMIT

--- myApp/polls/admin.py
+++ myApp/polls/admin.py
@@ -1,5 +1,19 @@
 from django.contrib import admin
-from .models import Pertanyaan
+from .models import Pertanyaan, Pilih
 
 # Register your models here.
-admin.site.register(Pertanyaan)
+
+class PilihInline(admin.TabularInline):
+    model = Pilih
+    extra = 3
+class PertanyaanAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['pertanyaan_text']}),
+        ('Date information', {'fields': ['pub_date'],'classes':'collapse'}),
+    ]
+    inlines = [PilihInline]
+    list_display = ('id','pertanyaan_text', 'pub_date', 'was_publish_terbaru')
+    list_filter = ['pub_date']
+    search_fields = ['pertanyaan_text']
+
+admin.site.register(Pertanyaan, PertanyaanAdmin)
